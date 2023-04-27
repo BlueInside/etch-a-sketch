1c5407a19338f9ca1d046b8e7fd92d670c06d63d
Karol <kpulawski13@gmail.com>
2023-04-27 10:16:49 +0100

    clean up the code

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-//Create grid 16x16px
-// use divs in container 16 divs in 16 rows
-// use for loop to iterate and create divs
-// we have to give them size otherwise they will be not visibleq
 const container = document.querySelector(".container");
 
-//button on top of the screen that asks user number squares per side for new grid;
-// how to restart page so it changes amount of squares when user enters input?
-// make a function- that fires after click event cleans container and makes new one?
+//Ask user for amount of squares
 const btn = document.querySelector("body > div.center > button");
 btn.addEventListener("click", () => {
   const squaresAmount = prompt("how many squares?");
   createSquares(squaresAmount);
-  // add hover effect so grid divs change color when mouse passes over
-  // select all divs queryselectorall? foreach element add eventlistener with hovering
-  //change div background using JS?
+  //Change divs bg on hover
   const squares = document.querySelectorAll(".column");
   squares.forEach((square) => {
     square.addEventListener("mouseover", () => {
@@ -24,6 +16,7 @@ btn.addEventListener("click", () => {
 
 function createSquares(squares) {
   container.innerHTML = "";
+  //Create grid squares x squares
   for (let i = 0; i < squares; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
