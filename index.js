995e6a000f6168f80c83944f9ee074cb5929bb53
Karol <kpulawski13@gmail.com>
2023-04-27 10:12:46 +0100

    add event listener to every square made by user

--- index.js
+++ index.js
@@ -13,20 +13,35 @@ for (let i = 0; i < 16; i++) {
     row.appendChild(column);
   }
 }
-// add hover effect so grid divs change color when mouse passes over
-// select all divs queryselectorall? foreach element add eventlistener with hovering
-//change div background using JS?
-const squares = document.querySelectorAll(".column");
-squares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    square.classList.add("color-me");
-  });
-});
 
 //button on top of the screen that asks user number squares per side for new grid;
 // how to restart page so it changes amount of squares when user enters input?
 // make a function- that fires after click event cleans container and makes new one?
-const btn = document.querySelector("body > div.center");
+const btn = document.querySelector("body > div.center > button");
 btn.addEventListener("click", () => {
-  const squaresAmount = alert("how many squares?");
+  const squaresAmount = prompt("how many squares?");
+  createSquares(squaresAmount);
+  // add hover effect so grid divs change color when mouse passes over
+  // select all divs queryselectorall? foreach element add eventlistener with hovering
+  //change div background using JS?
+  const squares = document.querySelectorAll(".column");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.classList.add("color-me");
+    });
+  });
 });
+
+function createSquares(squares) {
+  container.innerHTML = "";
+  for (let i = 0; i < squares; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let j = 0; j < squares; j++) {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
+    }
+  }
+}
