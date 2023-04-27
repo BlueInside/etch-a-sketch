0c7c238d3f88ff9b7aa2d9af088aa5d92d63f93a
Karol <kpulawski13@gmail.com>
2023-04-27 10:29:01 +0100

    Color only when button pressed!

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const container = document.querySelector(".container");
 //Ask user for amount of squares
 const btn = document.querySelector("body > div.center > button");
 btn.addEventListener("click", () => {
-  const squaresAmount = prompt("how many squares?");
+  const squaresAmount = prompt("how many squares? (100 max)");
   createSquares(squaresAmount);
   //Change divs bg on hover
   const squares = document.querySelectorAll(".column");
   squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-      square.classList.add("color-me");
+    square.addEventListener("mouseover", (e) => {
+      //Color only if button pressed!
+      if (e.buttons) square.classList.add("color-me");
     });
   });
 });
 
 function createSquares(squares) {
+  // set limit for the user input to max 100 squares
+  squares = squares > 100 ? 100 : squares;
+
   container.innerHTML = "";
   //Create grid squares x squares
   for (let i = 0; i < squares; i++) {
