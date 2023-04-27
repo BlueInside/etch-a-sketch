6f722a8816f6555743a3659644b485f9d6ea5bd6
Karol <kpulawski13@gmail.com>
2023-04-27 10:14:17 +0100

    remove iteration to create grid

--- index.js
+++ index.js
@@ -3,16 +3,6 @@
 // use for loop to iterate and create divs
 // we have to give them size otherwise they will be not visibleq
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  container.appendChild(row);
-  for (let j = 0; j < 16; j++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    row.appendChild(column);
-  }
-}
 
 //button on top of the screen that asks user number squares per side for new grid;
 // how to restart page so it changes amount of squares when user enters input?
